dfc9ce54aea7dbc55a1bbd4862889b170bf1ba92
Juan Luis Baptiste <juan.baptiste@gmail.com>
2017-11-08 21:13:02 -0500

    Renamed env variable DB_RESTORE_TARGET to for restore scripts RESTORE_TARGET.

--- scripts.d.examples/pre-restore/restore_full_backup.sh
+++ scripts.d.examples/pre-restore/restore_full_backup.sh
@@ -4,17 +4,17 @@ if [[ -n "$DB_DUMP_DEBUG" ]]; then
   set -x
 fi
 
-if [ -n ${DB_RESTORE_TARGET} ];
+if [ -n ${RESTORE_TARGET} ];
 then
-  backups_dir=$(dirname ${DB_RESTORE_TARGET})
-  backup_filename=$(basename ${DB_RESTORE_TARGET})
+  backups_dir=$(dirname ${RESTORE_TARGET})
+  backup_filename=$(basename ${RESTORE_TARGET})
   tmp_dir=$(mktemp -d)
   backup_files=""
   sql_dump=""
 
   >&2 echo "Getting contents of tarball"
   IFS_ORIG=${IFS}
-  backup_files="$(tar tvf ${DB_RESTORE_TARGET}|awk '{ print $6 }' )"
+  backup_files="$(tar tvf ${RESTORE_TARGET}|awk '{ print $6 }' )"
   backup_files="$(echo ${backup_files}|  tr '\n' ' ')"
   if [ "${backup_files}" == "" ];then
     >&2 echo "ERROR: Empty tarball!" && exit 1
@@ -22,11 +22,12 @@ then
 
   >&2 echo "Uncompressing tarball"
   cd /${tmp_dir}
-  tar zxf ${DB_RESTORE_TARGET}
+  tar zxf ${RESTORE_TARGET}
   [[ $? -gt 0 ]] && echo "Could not uncompress tarball!" && exit 1
 
   for i in ${backup_files}; do
-    #Get the sql dump and put it back on DB_RESTORE_TARGET as mysql-backup expects
+    #Get the sql dump and echo it back so DB_RESTORE_TARGET can be updated with the
+    # sql dump as mysql-backup expects
     if [ "${i/db_backup}" != ${i} ]; then
       >&2 echo "Found SQL dump tarball: ${i}"
       sql_dump=${i}
@@ -37,7 +38,7 @@ then
     fi
   done
 else
-  echo >&2 "ERROR: Backup file ${DB_RESTORE_TARGET} or restore directory does not exist!"
+  echo >&2 "ERROR: Backup file ${RESTORE_TAR} or restore directory does not exist!"
 fi
 
 #Echo the name of the sql dump file so the backup can continue as usual.
