e0eaef5b3bfe1f27b43facb0ec523455a40d69e7
Juan Luis Baptiste <juan.baptiste@gmail.com>
2017-11-08 23:53:16 -0500

    Restored latest version of backup script from branch hell.

--- scripts.d.examples/post-backup/backup_wordpress_root.sh
+++ scripts.d.examples/post-backup/backup_wordpress_root.sh
@@ -10,15 +10,23 @@ if [ -e ${DUMPFILE} ];
 then
   backups_dir=$(dirname ${DUMPFILE})
   tmp_dir=$(mktemp -d)
-  wordpress_files="${tmp_dir}/wordpress-${NOW}-files.tar.gz"
+  wordpress_files="wordpress-${NOW}-files.tar.gz"
   wordpress_full_backup="wordpress-${NOW}-full.tar.gz"
+  restore_dir="/restore"
+  mkdir ${restore_dir}
 
   echo "Backing up WordPress directory"
-  tar zcf ${wordpress_files} /var/www/html/wp-content
+  cd /${tmp_dir}
+  tar zcf ${wordpress_files} -C ${tmp_dir} /var/www/html/wp-content
   [[ $? -gt 0 ]] && echo "Could not compress WordPress directory!" && exit 1
 
+  echo "Copying SQL dump file"
+  cp ${DUMPFILE} ${tmp_dir}
+  [[ $? -gt 0 ]] && echo "Could not copy SQL dump!" && exit 1
+
   echo "Creating new tarball"
-  tar zcf ${tmp_dir}/${wordpress_full_backup} ${wordpress_files} ${DUMPFILE}
+  #cd ${restore_dir}
+  tar zcf ${restore_dir}/${wordpress_full_backup} *
   [[ $? -gt 0 ]] && echo "Could not create WordPress full backup file!" && exit 1
 
   echo "Moving new backup file to: ${DUMPDIR}"
