8813769e3b4e7cfc1db9ebba478494f9ac9e0669
Juan Luis Baptiste <juan.baptiste@gmail.com>
2018-04-20 19:20:18 -0500

    Do not hardcode backup filename and receive the service name as a parameter

--- scripts.d.examples/post-backup/backup_wordpress_root.sh
+++ scripts.d.examples/post-backup/backup_wordpress_root.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 # Backup a WordPress site (db + files).
 WWW_ROOT=${WWW_ROOT:-"/var/www/html/wp-content"}
+SERVICE_NAME=${SERVICE_NAME:-"wordpress"}
 
 if [[ -n "$DB_DUMP_DEBUG" ]]; then
   set -x
@@ -12,15 +13,15 @@ if [ -e ${DUMPFILE} ];
 then
   backups_dir=$(dirname ${DUMPFILE})
   tmp_dir=$(mktemp -d)
-  wordpress_files="wordpress-${NOW}-files.tar.gz"
-  wordpress_full_backup="wordpress-${NOW}-full.tar.gz"
+  wordpress_files="${SERVICE_NAME}-${NOW}-files.tar.gz"
+  wordpress_full_backup="${SERVICE_NAME}-${NOW}-full.tar.gz"
   restore_dir="/restore"
   mkdir ${restore_dir}
 
   echo "Backing up WordPress directory"
   cd /${tmp_dir}
   tar zcf ${wordpress_files} -C ${tmp_dir} ${WWW_ROOT}
-  [[ $? -gt 0 ]] && echo "Could not compress WordPress directory!" && exit 1
+  [[ $? -gt 0 ]] && echo "Could not compress files from ${SERVICE_NAME} directory!" && exit 1
 
   echo "Copying SQL dump file"
   cp ${DUMPFILE} ${tmp_dir}
@@ -29,7 +30,7 @@ then
   echo "Creating new tarball"
   #cd ${restore_dir}
   tar zcf ${restore_dir}/${wordpress_full_backup} *
-  [[ $? -gt 0 ]] && echo "Could not create WordPress full backup file!" && exit 1
+  [[ $? -gt 0 ]] && echo "Could not create ${SERVICE_NAME} full backup file!" && exit 1
 
   echo "Moving new backup file to: ${DUMPDIR}"
   mv ${restore_dir}/${wordpress_full_backup} ${DUMPDIR}
